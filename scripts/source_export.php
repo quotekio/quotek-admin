4f50701913a1e663878fbfed599df67325a08b53
Clément Gamé <quotekio@gmail.com>
2015-04-19 23:09:45 +0200

    Started GIT module Impl.

---
+++ scripts/source_export.php
@@ -0,0 +1,16 @@
+<?php
+
+$SOURCE_PATH="/usr/local/adam/";
+
+include (dirname(__FILE__) . "/../conf/config.inc.php");
+include ("strategy.php");
+
+$slist = getStrategies();
+
+foreach ($slist as $strat) {
+
+  
+
+}
+
+?>
