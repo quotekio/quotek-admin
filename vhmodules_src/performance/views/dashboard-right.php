faaa7f150ffc9020b3b344319e456b5fd67e6939
Clément Gamé <quotekio@gmail.com>
2015-04-28 00:40:46 +0200

    Work on performance dashboard view.

--- vhmodules_src/performance/views/dashboard-right.php
+++ vhmodules_src/performance/views/dashboard-right.php
@@ -6,42 +6,88 @@
 <div class="span6 app-headed-white-frame" style="height:268px">
 
   <div class="app-headed-frame-header">
-    <h4><?= $lang_array['performance']['trades_ratio_title'] ?></h4>
+    <h4><?= $lang_array['performance']['trades_stats'] ?></h4>
   </div>
 
-    <div style="text-align:center;margin-top:20px">
+    <div style="text-align:center;margin-top:14px">
 
     <div class="span4">
-      <div style="width:140px;height:140px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trdph">
-      </div>
       <div>
-        <h4>Jour</h4>
+        <b>Jour</b>
       </div>
-      <div id="performance-trdph-label" style="opacity:.6;position:relative;margin-top:-120px;font-size:40px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+      <div style="width:100px;height:100px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trdph">
+      </div>
+
+      <div id="performance-trdph-label" style="opacity:.6;position:relative;margin-top:-60px;font-size:25px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+
+      <div id="performance-pnlstats-daily" style="font-size:10px;margin-top:50px">
+
+        <table class="table table-bordered">
+          <tr>
+            <th>PNL (AVG)</th>
+            <th>PNL-P (AVG)</th>
+          </tr>
+          <tr>
+            <td id="apnl-daily">0</td>
+            <td id="apnl-p-daily">0</td>
+          </tr>
+        </table>
+      </div>
     </div>
 
-    
+
     <div class="span4">
-      <div style="width:140px;height:140px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trwph">
-      </div>
       <div>
-        <h4>Semaine</h4>
+        <b>Semaine</b>
       </div>
-      <div id="performance-trwph-label" style="opacity:.6;position:relative;margin-top:-120px;font-size:40px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+      <div style="width:100px;height:100px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trwph">
+      </div>
+
+      <div id="performance-trwph-label" style="opacity:.6;position:relative;margin-top:-60px;font-size:25px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+
+
+      <div id="performance-pnlstats-weekly" style="font-size:10px;margin-top:50px">
+        <table class="table table-bordered">
+          <tr>
+            <th>PNL (AVG)</th>
+            <th>PNL-P (AVG)</th>
+          </tr>
+          <tr>
+            <td id="apnl-weekly">0</td>
+            <td id="apnl-p-weekly">0</td>
+          </tr>
+        </table>
+      </div>
     </div>
 
     <div class="span4" style="text-align:center">
-      <div style="width:140px;height:140px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trmph">
-      </div>
       <div>
-        <h4>Mois</h4>
+        <b>Mois</b>
+      </div>
+      <div style="width:100px;height:100px;opacity:.6;margin-left:auto;margin-right:auto" id="performance-trmph">
       </div>
 
-      <div id="performance-trmph-label" style="opacity:.6;position:relative;margin-top:-120px;font-size:40px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+      <div id="performance-trmph-label" style="opacity:.6;position:relative;margin-top:-60px;font-size:25px;font-weight:bold;width:140px;left:50%;margin-left:-70px"></div>
+
+
+      <div id="performance-pnlstats-monthly" style="font-size:10px;margin-top:50px">
+        <table class="table table-bordered">
+          <tr>
+            <th>PNL (AVG)</th>
+            <th>PNL-P (AVG)</th>
+          </tr>
+          <tr>
+            <td id="apnl-monthly">0</td>
+            <td id="apnl-p-monthly">0</td>
+          </tr>
+        </table>
+      </div>
+
+
 
 
     </div>
-   
+
   </div>
 
 
