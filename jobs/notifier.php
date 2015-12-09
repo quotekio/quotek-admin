afb05b8beb74b35b1a11efe6b9b8cdd9f17999e4
Clément Gamé <quotekio@gmail.com>
2015-12-09 13:43:18 +0100

    Down Notifier is finished, now needs to implement reports notifier.

--- jobs/notifier.php
+++ jobs/notifier.php
@@ -1,7 +1,19 @@
 <?php
 
+//** EMAIL TEMPLATES **//
+$shutdown_email = array();
+$shutdown_email['subject'] = '[QUOTEK] Service Robot service is down';
+$shutdown_email['body'] = 'Your quotek robot was detected to be down at {{DATE}}';
+
+$headers = "From: notifier-noreply@quotek.io\n";
+$headers .='Content-Type: text/plain; charset="UTF-8"'."\n";
+$headers .="Content-Transfer-Encoding: 8bit\n\n";
+
 $period=60;
 $exec = function() {
+
+  global $shutdown_email;
+  global $headers;
 
   //checks notification Options
   $cfg = getActiveCfg();
@@ -10,7 +22,19 @@ $exec = function() {
   if ( $cfg->notify_shutdown == 1 && $actl->mode == 'off' ) {
     // Sends a shutdown notification
     echo "ADAM DOWN\n";
+ 
+    $shutdown_email['body'] = str_replace("{{DATE}}" , date("H:i:s"), $shutdown_email['body']);
+
+    mail($cfg->notify_to , 
+    	  $shutdown_email['subject'], 
+    	  $shutdown_email['body'],
+    	  $headers,
+    	  "-fnotifier-noreply@quotek.io");
+
   }
+
+
+
 
 };
 
