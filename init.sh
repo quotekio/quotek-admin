a7468c3c170883435cd456464b022907d2efa02b
Clément Gamé <quotekio@gmail.com>
2016-07-23 17:28:32 +0000

    Improvements on self inst.

--- init.sh
+++ init.sh
@@ -3,23 +3,24 @@
 
 function common() {
   php composer.phar install
+
+  echo 'www-data ALL=(ALL) NOPASSWD: /usr/bin/pkill, NOPASSWD: /usr/local/qate/bin/qate, NOPASSWD: /bin/kill, NOPASSWD: /bin/echo, NOPASSWD: /usr/bin/gdb, NOPASSWD: /usr/bin/screen, NOPASSWD: /etc/init.d/qate, NOPASSWD: /bin/sh' >> /etc/sudoers
+
+  tar -xvzf  quotek_gitinit.tgz -C/
+  chown -R www-data /quotek
 }
 
 
 function nginx() {
-
   apt-get -y update
   apt-get -y install nginx php-fpm php7.0-sqlite php-cli php-curl
   common
-  tar -xvzf  quotek_gitinit.tgz -C/
-  chown -R www-data /quotek
 
   cp ./install/etc/nginx/qwc.conf /etc/nginx/sites-available/
   ln -s /etc/nginx/sites-available/qwc.conf /etc/nginx/sites-enabled/qwc.conf
 
   unlink /etc/nginx/sites-enabled/default
   /etc/init.d/nginx restart
-
 }
 
 
