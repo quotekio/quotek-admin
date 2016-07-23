2622f0507089d1349e6e13f836f480a96405a2e3
Clément Gamé <quotekio@gmail.com>
2016-07-23 15:01:27 +0000

    Improved install procedure for QWC.

--- init.sh
+++ init.sh
@@ -9,13 +9,16 @@ function common() {
 function nginx() {
 
   apt-get -y update
-  apt-get -y install nginx php-fpm php7.0-sqlite php-cli
+  apt-get -y install nginx php-fpm php7.0-sqlite php-cli php-curl
   common
   tar -xvzf  quotek_gitinit.tgz -C/
   chown -R www-data /quotek
 
   cp ./install/etc/nginx/qwc.conf /etc/nginx/sites-available/
   ln -s /etc/nginx/sites-available/qwc.conf /etc/nginx/sites-enabled/qwc.conf
+
+  unlink /etc/nginx/sites-enabled/default
+  /etc/init.d/nginx restart
 
 }
 
