88e460dbc7b8c49a2f8d9c5814ecc35dc82766df
Clément Gamé <quotekio@gmail.com>
2018-04-08 17:38:40 +0200

    php-pgsql dep was missing, also added sqlite3 pkg.

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ function nginx() {
   export DEBCONF_NONINTERACTIVE_SEEN=true
 
   apt-get -y update
-  apt-get -y install nginx php-fpm php-sqlite3 php-cli php-curl php-common
+  apt-get -y install nginx sqlite3 php-fpm php-pgsql php-sqlite3 php-cli php-curl php-common
 
   cp ./install/etc/nginx/qwc.conf /etc/nginx/sites-available/
   ln -s /etc/nginx/sites-available/qwc.conf /etc/nginx/sites-enabled/qwc.conf
